ee38ce54866d21b64b61932cb7144d6d7eee6c65
Bryan <93453907+blopez024@users.noreply.github.com>
2025-04-01 19:00:14 -0700

    The querySelector() method returns the first element that matches a CSS selector. The script file targets the <h2>, <h3>, and <h4> tag using the querySelector ('h2'), ('h3'), and ('h4')

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
+// getElementById
 let titleElement = document.getElementById('title');
 console.log(titleElement);
+
+// querySelector
+let h2Element = document.querySelector('h2');
+let h3Element = document.querySelector('h3');
+let h4Element = document.querySelector('h4');
+
+console.log(h2Element);
+console.log(h3Element);
+console.log(h4Element);
